b21e8b3ff4ae68746cf074b9c09930f2ce150ba4
Rahmat Ardiansyah <artsbymat@gmail.com>
2025-02-21 15:26:09 +0700

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,21 @@
+export const SITE_TITLE = "artsbymat";
+export const SITE_DESCRIPTION =
+  `Astro + TailwindCSS with support for RSS, sitemap, MD/MDX blog posts static content.`.trim();
+
+export const KNOWN_TECH =
+  `HTML, CSS, Bootstrap, Tailwind, Javascript, Typescript, React, Astrojs, Nextjs, MariaDB, MongoDB, Firebase, NPM, Figma, Postman, Git, Linux Shell.`.split(
+    ",",
+  );
+export const ABOUT_ME =
+  `Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.`.trim();
+export const GITHUB_USERNAME = "artsbymat";
+export const QUOTE = "Programmer, Gamer, and Problem Solver";
+export const NAV_LINKS: Array<{ title: string; href?: string }> = [
+  {
+    title: "Blog",
+  },
+  {
+    title: "Github",
+    href: "//github.com/" + GITHUB_USERNAME,
+  },
+];
