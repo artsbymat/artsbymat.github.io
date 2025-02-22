57e694da304dee5c2cb6a6da0372aa1d7a194a86
Rahmat Ardiansyah <artsbymat@gmail.com>
2025-02-22 16:00:44 +0700

    upgrade: astro, tailwind, and prettier config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,19 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
-    site: "https://artsbymat.github.io",
-    integrations: [mdx(), sitemap(), tailwind()],
-    markdown: {
-        shikiConfig: {
-            theme: "github-dark-high-contrast",
-        },
-    },
+  output: "static",
+  site: "https://artsbymat.github.io",
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark-high-contrast"
+    }
+  }
 });
--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ export const SITE_DESCRIPTION =
 
 export const KNOWN_TECH =
   `HTML, CSS, Bootstrap, Tailwind, Javascript, Typescript, React, Astrojs, Nextjs, MariaDB, MongoDB, Firebase, NPM, Figma, Postman, Git, Linux Shell.`.split(
-    ",",
+    ","
   );
 export const ABOUT_ME =
   `Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.`.trim();
@@ -12,10 +12,10 @@ export const GITHUB_USERNAME = "artsbymat";
 export const QUOTE = "Programmer, Gamer, and Problem Solver";
 export const NAV_LINKS: Array<{ title: string; href?: string }> = [
   {
-    title: "Blog",
+    title: "Blog"
   },
   {
     title: "Github",
-    href: "//github.com/" + GITHUB_USERNAME,
-  },
+    href: "//github.com/" + GITHUB_USERNAME
+  }
 ];
