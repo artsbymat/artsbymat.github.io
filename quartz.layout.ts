3ab088484cd75b254b02c3e1ff8511dde322d341
Rahmat Ardiansyah <artsbymat@gmail.com>
2026-01-09 18:16:41 +0700

    Quartz sync: Jan 9, 2026, 6:16 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -33,12 +33,9 @@ export const defaultContentPageLayout: PageLayout = {
       ],
     }),
     Component.Explorer(),
+    Component.DesktopOnly(Component.Graph()),
   ],
-  right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+  right: [Component.DesktopOnly(Component.TableOfContents()), Component.Backlinks()],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
